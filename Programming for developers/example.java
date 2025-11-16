a185c55df538bd67995721c6b6940aae841497ab
Nelish Maharjan <nelishmaharjan16@icloud.com>
2025-11-16 09:03:45 +0000

    dsa

--- Programming for developers/example.java
+++ Programming for developers/example.java
@@ -1,13 +1,26 @@
-class example {
-    static int Factorial(int n){
-        if(n != 0)
-            return n * Factorial(n - 1);
-        else
-            return 1;
-    }
-    public static void main(String args[]){
-        int num = 4, result;
-        result = Factorial(num);
-        System.out.println("Factorial of " + num + " is: " + result);
+// class example {
+//     static int Factorial(int n){
+//         if(n != 0)
+//             return n * Factorial(n - 1);
+//         else
+//             return 1;
+//     }
+//     public static void main(String args[]){
+//         int num = 4, result;
+//         result = Factorial(num);
+//         System.out.println("Factorial of " + num + " is: " + result);
+//     }
+// }
+
+//Java Program to illustrate the use of mutli dimensional array
+class Testarray3{
+    public static void main(String[] args) {
+        int arr[][] = { {1, 2, 3}, {2, 4, 5}, {4, 4, 5} };
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ Programming for developers/learning2.java
@@ -0,0 +1,12 @@
+public class learning2{
+    public static void display(int y[]) {
+        System.out.println(y[0]);
+        System.out.println(y[1]);
+        System.out.println(y[2]);
+        
+    }
+    public static void main(String[] args) {
+        int x[] = {1, 2, 3};
+        display(x);
+    }
+}
